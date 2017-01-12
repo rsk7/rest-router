44bc3962cae12fc6423f861ed16ec9dc923eee48
rsk7 <ravikrishnasingh@gmail.com>
2017-01-12 16:23:39 -0500

    adding query find to get

--- rest-routes.js
+++ rest-routes.js
@@ -1,3 +1,38 @@
+function createFindQuery(obj) {
+  if (options && options.idConverter) {
+    var findQuery = Object.assign({}, obj);
+    var keys = Object.keys(findQuery);
+
+    // TODO: make finding idKeys configurable
+    var idKeys = keys.filter(k => k.endsWith('Id'));
+
+    for(var i = 0; i < idKeys.length; i++) {
+      var currentValue = findQuery[idKeys[i]];
+      try {
+        findQuery[idKeys[i]] = options.idConverter(currentValue);
+      } catch(err) {
+        // ignore conversion failure
+        // remove key from query
+        delete findQuery[idKeys[i]];
+      }
+    }
+
+    return findQuery;
+  }
+}
+
+function findQuery(req, res, next) {
+  var findQuery = {};
+  if (Object.keys(req.query).length) {
+    findQuery = createFindQuery(req.query);
+    if (!Object.keys(findQuery).length) {
+      return res.send([]);
+    }
+  }
+  req.findQuery = findQuery;
+  next();
+}
+
 /*
  * Given an express router and a mongoose model
  * Returns the router with GET, PUT, POST and DELETE
@@ -5,8 +40,8 @@
  */
 function rest(router, Model) {
   // GET
-  router.get("/", function(req, res, next) {
-    Model.find({}, function(err, modelInstances) {
+  router.get("/", findQuery, function(req, res, next) {
+    Model.find(req.findQuery, function(err, modelInstances) {
       if (err) next(err);
       else res.send(modelInstances);
     });
